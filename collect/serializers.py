59710c569d932ce6ec506824c7d522e5129bfc35
Bhavesh Praveen <bhaveshpraveen10@gmail.com>
2019-08-16 17:46:03 +0530

    Team serializers (3rd endpoint)

--- collect/serializers.py
+++ collect/serializers.py
@@ -1,7 +1,7 @@
 
 from rest_framework import serializers
 
-from collect.models import FileUpload, Data
+from collect.models import FileUpload, Data, TeamFileUpload, Team
 
 
 class FileUploadSerializer(serializers.ModelSerializer):
@@ -14,3 +14,16 @@ class DataSerializer(serializers.ModelSerializer):
     class Meta:
         model = Data
         fields = ('data', 'order_placed')
+
+
+class TeamFileUploadSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', 'file')
+        model = TeamFileUpload
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('team_name', 'manager_name', 'manager_phone_number')
+        model = Team
+
