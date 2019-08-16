d231b48afbacc372670039c0f0a023eedb651950
Bhavesh Praveen <bhaveshpraveen10@gmail.com>
2019-08-16 17:47:05 +0530

    remove default pagination

--- atlan_task/settings.py
+++ atlan_task/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
