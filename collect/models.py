fd2ecfac07b715b654fb562e639e271131b049a6
Bhavesh Praveen <bhaveshpraveen10@gmail.com>
2019-08-16 17:40:43 +0530

    teams models and migrations

--- collect/models.py
+++ collect/models.py
@@ -12,6 +12,9 @@ class FileUpload(models.Model):
     def __str__(self):
         return f'id: {self.id}, task_id: {self.task_id}'
 
+    class Meta:
+        ordering = ('-id', )
+
 
 class Data(models.Model):
     file_upload = models.ForeignKey('FileUpload', related_name='data', on_delete=models.CASCADE)
@@ -21,17 +24,39 @@ class Data(models.Model):
     def __str__(self):
         return f'pk = {self.pk}, data={self.data}'
 
+    class Meta:
+        ordering = ('-id', )
+
 
 class TeamFileUpload(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='file_uploads')
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='team_file_uploads')
     task_id = models.CharField(verbose_name='celery_task_id', max_length=150, blank=True)
     file = models.FileField()
 
+    def __str__(self):
+        return f'id: {self.id}, task_id: {self.task_id}'
+
+    class Meta:
+        ordering = ('-id',)
+
 
 class Team(models.Model):
-    team_id = models.PositiveIntegerField(primary_key=True)
-    players = JSONField()
-    manager = JSONField()
+    file_upload = models.ForeignKey('TeamFileUpload', on_delete=models.CASCADE, related_name='teams')
+    team_name = models.CharField(verbose_name='Team name', max_length=100)
+    manager_name = models.CharField(verbose_name='Manager name', max_length=100)
+    manager_phone_number = models.CharField(verbose_name='Phone number', max_length=15)
+
+    def __str__(self):
+        return f'{self.id}: {self.name}'
+
+    class Meta:
+        ordering = ('-id', )
+
+
+# class Team(models.Model):
+#     team_id = models.PositiveIntegerField(primary_key=True)
+#     players = JSONField()
+#     manager = JSONField()
 
 
 # class Team(models.Model):
